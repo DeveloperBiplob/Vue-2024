c30acfce71ef99b9f9574f03c799f86157eb9100
BIPLOB <developerbiplob.bd@gmail.com>
2024-11-15 21:36:02 +0600

    Vue Project Setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <HelloWorld msg="Hello from Main Components" phone="01643381009" :names="names"/>
+</div>
+</template>
+
+<script setup>
+  import HelloWorld from "./components/HelloWold.vue"
+  console.log("HelloWorld", HelloWorld)
+
+  const names = [
+    'biplob',
+    'mastura',
+    'Meherim',
+  ]
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWold.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <div>
+        <h1>{{ msg }} - {{ phone }}</h1>
+        <ul>
+            <li v-for="name in names" :key="name">{{ name }}</li>
+        </ul>
+
+        <button @click="increment">Increments</button>
+        <h5>{{ count }}</h5>
+    </div>
+</template>
+
+<script setup>
+
+import {ref} from "vue"
+
+    defineProps([
+        "msg",
+        "phone",
+        "names",
+    ]);
+
+    let count = ref(0);
+
+    const increment = () => {
+        count.value ++;
+        console.log("count", count.value);
+    }
+
+</script>
+
+<style lang="" scoped>
+
+</style>
